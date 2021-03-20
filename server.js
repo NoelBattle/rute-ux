63ba302f22ae9b40e9634acf48d6aa676e7ab23b
Tab <tab@Noels-MBP.attlocal.net>
2021-03-20 15:45:21 -0700

    test

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000
 
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 
-app.get('/', (req, res) => {
-   res.send("Hello World!");
-})
-
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
