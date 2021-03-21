5a16e413b70b30b67b0ef066a55db17bcd398a84
Tab <tab@Noels-MBP.attlocal.net>
2021-03-20 19:17:11 -0700

    test3

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path')
 const app = express();
 const port = process.env.PORT || 8080
 const publicPath = path.join(__dirname, '..', 'public');
-app.use(express.static(publicPath));
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
  });
