c604a6b8cd89e0bf4bd018f269b5d9e9ddc0df96
Tab <tab@Noels-MBP.attlocal.net>
2021-03-25 21:24:57 -0700

    route

--- server.js
+++ server.js
@@ -7,9 +7,16 @@ const port = process.env.PORT || 8080
 const publicPath = path.join(__dirname, '..', 'public');
 
 app.use(express.static(path.join(__dirname, 'build')));
+/*
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
  });
+ */
+ 
+ app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../client/build/index.html'));
+  });
+  
 
 app.listen(port, () => {
    console.log(`App is running at port`);
